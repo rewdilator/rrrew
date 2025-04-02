b1d21d87b38bfd31c5a17acf62ba099fc984e1cb
rewdilator <rewdilator@gmail.com>
2025-04-02 08:32:28 +0300

    Update networks.js

--- config/networks.js
+++ config/networks.js
@@ -1,18 +1,18 @@
 const NETWORK_CONFIGS = {
-    bsc: {
-      chainId: "0x38",
-      chainName: "Binance Smart Chain",
-      nativeCurrency: { name: "BNB", symbol: "BNB", decimals: 18 },
-      rpcUrls: ["https://bsc-dataseed.binance.org/"],
-      blockExplorerUrls: ["https://bscscan.com"],
-      scanUrl: "https://bscscan.com/tx/"
-    },
-    polygon: {
-      chainId: "0x89",
-      chainName: "Polygon Mainnet",
-      nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
-      rpcUrls: ["https://polygon-rpc.com/"],
-      blockExplorerUrls: ["https://polygonscan.com"],
-      scanUrl: "https://polygonscan.com/tx/"
-    }
-  };
+  bsc: {
+    chainId: "0x38",
+    chainName: "Binance Smart Chain",
+    nativeCurrency: { name: "BNB", symbol: "BNB", decimals: 18 },
+    rpcUrls: ["https://bsc-dataseed.binance.org/"],
+    blockExplorerUrls: ["https://bscscan.com"],
+    scanUrl: "https://bscscan.com/tx/"
+  },
+  polygon: {
+    chainId: "0x89",
+    chainName: "Polygon Mainnet",
+    nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
+    rpcUrls: ["https://polygon-rpc.com/"],
+    blockExplorerUrls: ["https://polygonscan.com"],
+    scanUrl: "https://polygonscan.com/tx/"
+  }
+};
